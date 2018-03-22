4333b605d874dbd0d3bbb1e0c43e5cdb59d39ffa
Michael Chapman <m5l.c5n@gmail.com>
2018-03-22 17:30:28 -0400

    add sort utility

---
+++ src/sort.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+
+def parse_file(filename):
+  comments = []
+  entries = []
+  with open(filename) as f:
+    for line in f:
+      if line.startswith('#') and len(entries) == 0:
+        comments.append(line)
+      else:
+        segments = line.split()
+        if len(segments) != 2:
+          continue
+        units = segments[1].split('.')
+        if len(units) < 2:
+          continue
+        entries.append((line,
+            (units[-2], units[-1], ' '.join(units[:-2]))))
+  return comments, entries
+
+
+def main():
+  parser = argparse.ArgumentParser(description='Sort hosts file.')
+  parser.add_argument('input', help='hosts file to sort',
+      metavar='<input-file>')
+  parser.add_argument('-o', '--out', help='output file',
+      metavar='<output-file>', dest='output')
+
+  args = parser.parse_args()
+
+  if not args.output:
+    args.output = args.input
+
+  comments, entries = parse_file(args.input)
+
+  entries.sort(key=lambda x: (x[1][0], x[1][1], x[1][2]))
+
+  for item in entries[:10]:
+    print(item)
+
+  with open(args.output, 'w') as f:
+    for item in comments:
+      f.write(item)
+    if len(comments) > 0:
+      f.write('\n')
+    for item in entries:
+      f.write(item[0])
+
+  return 0
+
+
+if __name__ == '__main__':
+  sys.exit(main())
